26bcbaf7773287ca0ee20dceef93db0cfe08b6bd
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-04-17 11:37:14 +0100

    working code including exceptions

--- src/main/scala/nthLargest.scala
+++ src/main/scala/nthLargest.scala
@@ -1,10 +1,15 @@
 class NumberNotPossibleException extends Exception
 
-object nthLargest extends App {
+object nthLargest {
 
-  def apply(input: List[Int], nth : Int) : Any = {
-    val uniqueNums = input.distinct
+  def apply(input: List[Int], nth: Int): Any = {
+
+    val uniqueNums = input.distinct.sorted
+
+    if (nth <= uniqueNums.length) {
       uniqueNums(uniqueNums.length - nth)
+    } else {
+      throw new NumberNotPossibleException
+    }
   }
-  
 }
--- src/test/scala/nthLargestSpec.scala
+++ src/test/scala/nthLargestSpec.scala
@@ -12,6 +12,10 @@ class nthLargestSpec extends WordSpec with MustMatchers {
       nthLargest(List(1, 2), 1) mustEqual 2
     }
 
+    "return 2 when given List(2,1) and 1" in {
+      nthLargest(List(2, 1), 1) mustEqual 2
+    }
+
     "return 1 when given List(1,2) and 2" in {
       nthLargest(List(1, 2), 2) mustEqual 1
     }
@@ -23,8 +27,31 @@ class nthLargestSpec extends WordSpec with MustMatchers {
     "return 2 when given List(1,1,2,2) and 1" in {
       nthLargest(List(1, 1, 2, 2), 1) mustEqual 2
     }
+
     "return 3 when given List(3,3,4,4,5,5) and 3" in {
       nthLargest(List(3, 3, 4, 4, 5, 5), 3) mustEqual 3
+    }
+
+    "return 4 when given List(5,4,5,4,3,5) and 2" in {
+      nthLargest(List(5, 4, 5, 4, 3, 5), 2) mustEqual 4
+    }
+
+    "throw an error when given List() and 1" in {
+      intercept[NumberNotPossibleException] {
+        nthLargest(List(), 1)
+      }
+    }
+
+    "throw an error when given List(1) and 2" in {
+      intercept[NumberNotPossibleException] {
+        nthLargest(List(1), 2)
+      }
+    }
+
+    "throw an error when given List(1,2) and 3" in {
+      intercept[NumberNotPossibleException] {
+        nthLargest(List(1, 2), 3)
+      }
     }
   }
 }
